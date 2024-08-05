199542e5b26b7f9d7ed89b7e77cca7571a7c711e
rajesh-orrizonte <rajesh.pilli@orrizonte.in>
2024-08-05 12:26:42 +0530

    Home page UI changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import { MdEdit } from "react-icons/md";
 import { MdDeleteOutline } from "react-icons/md";
 import axios from "axios";
-import { FaUserCircle } from "react-icons/fa";
+import { FaRegEdit, FaUserCircle } from "react-icons/fa";
 import { AiOutlineLogout } from "react-icons/ai";
 
 const Home = () => {
@@ -169,6 +169,10 @@ const Home = () => {
     setShowAddTodo(false);
   };
 
+  const goToLoginPage = () => {
+    navigate("/login");
+  };
+
   return (
     <div className="h-[100vh] w-[100vw] home-container">
       <navbar className="flex justify-between items-center bg-slate-100 p-3 shadow">
@@ -195,11 +199,20 @@ const Home = () => {
               className="ml-5 cursor-pointer"
             />
           </div>
-        ) : null}
+        ) : (
+          <>
+            <button
+              className="hover:bg-purple-700 px-3 py-1 hover:text-white rounded border-2 border-purple-700 text-purple-700 font-semibold"
+              onClick={goToLoginPage}
+            >
+              Login
+            </button>
+          </>
+        )}
       </navbar>
       {showAddTodo ? (
         <form className="mt-16 px-20" onSubmit={handleAddTodoForm}>
-          <h1 className="text-blue-700 font-bold text-2xl mb-5">
+          <h1 className="text-purple-700 font-bold text-2xl mb-5">
             {showEditButton ? "Update Todo" : "Todo Details"}
           </h1>
           <div className="flex  justify-start items-center mobile-view">
@@ -241,21 +254,21 @@ const Home = () => {
           <div className="mt-5">
             {showEditButton ? (
               <button
-                className="bg-blue-600 py-2 px-4 text-white rounded-lg  border-2 font-semibold "
+                className="bg-purple-700 py-2 px-4 text-white rounded-lg  border-2 font-semibold "
                 type="submit"
               >
                 Update
               </button>
             ) : (
               <button
-                className="bg-blue-600 py-2 px-4 text-white rounded-lg  border-2 font-semibold "
+                className="bg-purple-700 px-3 py-1 text-white rounded-lg border-2 border-purple-700  font-semibold"
                 type="submit"
               >
                 Save
               </button>
             )}
             <button
-              className="ml-3 py-2 px-4 text-blue-600 rounded-lg  border-2 border-blue-600  font-semibold"
+              className="ml-3 py-1 px-4 text-purple-700 rounded-lg  border-2 border-purple-700  font-semibold"
               onClick={handleCancel}
             >
               Close
@@ -264,40 +277,50 @@ const Home = () => {
         </form>
       ) : (
         <>
-          <h1 className="font-bold text-2xl text-green-600 text-center mt-10">
+          <h1 className="font-bold text-2xl text-purple-700 text-center mt-10">
             Welcome to TODOS DASHBOARD
           </h1>
           <div className="flex flex-col items-center mt-10">
             <div className="w-[85%] m-3 flex justify-end">
               <button
-                className="bg-blue-600 p-2 text-white rounded-lg  border-2 font-semibold self-end"
+                className="bg-purple-700 p-2 text-white rounded-lg  border-2 font-semibold self-end"
                 onClick={handleAddTodo}
               >
                 Add TODO
               </button>
             </div>
             <table className="w-[85%] ">
-              <thead>
-                <tr>
-                  <th className="border-2 border-gray-700">Id</th>
-                  <th className="border-2 border-gray-700">Description</th>
-                  <th className="border-2 border-gray-700">Status</th>
-                  <th className="border-2 border-gray-700">Action</th>
+              <thead className="text-white bg-purple-700 font-bold text-xl">
+                <tr className="border-2 border-purple-700">
+                  <th>Id</th>
+                  <th>Description</th>
+                  <th>Status</th>
+                  <th>Action</th>
                 </tr>
               </thead>
-              <tbody className="text-center ">
+              <tbody className="text-center">
                 {allTodos && allTodos.length > 0
                   ? allTodos.map((todo) => (
-                      <tr className="border-2 border-gray-700" key={todo.id}>
+                      <tr className="border-2 border-purple-700" key={todo.id}>
                         <td>{todo.id}</td>
                         <td>{todo.description}</td>
-                        <td>{todo.status}</td>
+                        <td
+                          className={`${
+                            todo.status === "Pending"
+                              ? "text-red-700 font-bold"
+                              : todo.status === "In Progress"
+                              ? "text-yellow-700 font-bold"
+                              : "text-green-700 font-bold"
+                          }`}
+                        >
+                          {todo.status}
+                        </td>
                         <td>
                           <div className="flex justify-center">
-                            <MdEdit
+                            <FaRegEdit
                               color="blue"
                               size={20}
-                              className="mr-2 cursor-pointer"
+                              className="mr-4 cursor-pointer"
                               onClick={() => handleTodoEdit(todo)}
                             />
                             <MdDeleteOutline
